998d674628853fb096b6b835bf58caa8e3ca8db4
Giacomo Gasperini <giacomo.gasperini@gmail.com>
2023-10-15 15:48:26 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="" @click="layoutsStore.hideAll">
+    <LayoutTheHeader />
+    <slot />
+    <LayoutTheFooter />
+  </div>
+</template>
+<script setup lang="ts">
+import { useLayoutStore } from '~/store/layouts';
+useHead({
+  bodyAttrs: {
+    class: ``,
+  },
+});
+const layoutsStore = useLayoutStore();
+</script>
